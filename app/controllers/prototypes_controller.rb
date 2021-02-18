0989332d76bb6a09fc8e50014bba8c129023689e
jack1417 <78401554+jack1417@users.noreply.github.com>
2021-02-18 16:55:06 +0900

    issue1の修正の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:create, :new, :edit, :destory]
-  before_action :move_to_index, exept: [:index, :show]
+  before_action :move_to_index, except: [:index]
   def index
    @prototypes = Prototype.all
   end
@@ -56,8 +56,10 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
 end
+
